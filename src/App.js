d48a4a6657f2a95971fd8748800d1fde8973116b
Mia Ferrari <miaa_ferrari@hotmail.com>
2021-10-18 14:37:50 -0300

    feat: button component done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import GlobalStyled from "./styles/global";
+import { Button } from "./components/Button";
 
 function App() {
   return (
     <div>
       <GlobalStyled />
+      <Button whiteSchema >Adicionar</Button>
+
     </div>
   );
 }
---
+++ src/components/Button/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+    background: ${(props) => props.whiteSchema ? '#f5f5f5' : '#3B4977'};
+    color: ${(props) => props.whiteSchema ? '#3B4977' : '#f5f5f5'};
+    border-radius: 8px;
+    border: 2px solid #3B4977;
+    font-family: 'Inter', sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 19px;
+    letter-spacing: -0.017em;
+    margin: 15px 0px;
+    width: 295px;
+    height: 50px;
+    padding: 0px 30px;
+    transition: 0.5s;
+
+    :hover {
+        opacity: 0.6;
+    }
+
+
+`;
