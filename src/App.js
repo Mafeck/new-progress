d3d54be8880967a1ae25427d0a2c2d8697f767af
Mia Ferrari <miaa_ferrari@hotmail.com>
2021-10-18 14:40:35 -0300

    feat: button component removed from App. I put there to visualization.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import GlobalStyled from "./styles/global";
-import { Button } from "./components/Button";
 
 function App() {
   return (
     <div>
       <GlobalStyled />
-      <Button whiteSchema >Adicionar</Button>
+     
 
     </div>
   );
