afe63b43026e0aa7a807fb731db621e50fb74ba3
patrickgsda <patrickarruda30@gmail.com>
2021-10-19 19:49:37 -0300

    test: feature login ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Login from "./pages/Login";
 import GlobalStyled from "./styles/global";
 
 function App() {
   return (
     <div>
       <GlobalStyled />
-      <Login />
     </div>
   );
 }
