7a458fb9869c85603c732f7aff48ec4485a4ecab
patrickgsda <patrickarruda30@gmail.com>
2021-10-18 23:40:10 -0300

    Teste Input OK.

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import { TextField } from '@material-ui/core';
+import React from "react";
+import { TextField } from "@material-ui/core";
 
-export const Input = ({ label, register, name, error, helperText, ...rest }) =>     {
-    return (
-        <>
-            <TextField
-                id="outlined-username"
-                label={label} 
-                variant="outlined"
-                margin="normal"
-                size="small"
-                fullWidth
-                style={{
-                  background: "var(--grey-2)",
-                  color: "var(--color-text)",
-                  opacity: " 0.7",
-                }}
-                {...register(name)}
-                error={error}
-                helperText={helperText}
-                {...rest}
-              />    
-        </>
-    )
+export const Input = ({
+  label,
+  register,
+  name,
+  error,
+  helperText,
+  ...rest
+}) => {
+  return (
+    <TextField
+      id="outlined-username"
+      label={label}
+      variant="outlined"
+      margin="normal"
+      size="small"
+      fullWidth
+      style={{
+        background: "var(--grey-2)",
+        color: "var(--color-text)",
+        opacity: " 0.7",
+      }}
+      {...register(name)}
+      error={error}
+      helperText={helperText}
+      {...rest}
+    />
+  );
 };
