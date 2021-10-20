688fcff8d6c753072d4ef3b79d2c4f7454c47b23
marcusvl <marcusvlcastro@gmail.com>
2021-10-20 16:37:31 -0300

    teste

--- src/pages/Groups/index.jsx
+++ src/pages/Groups/index.jsx
@@ -1,11 +1,10 @@
 import Header from "../../components/Header";
-import jwtDecode from "jwt-decode";
 import { useContext } from "react";
 import { GroupsContext } from "../../Providers/Groups";
-import Card from '../../components/Card';
+import Card from "../../components/Card";
 import { useEffect } from "react";
-import api from '../../services/api';
-import { UserContext } from '../../Providers/User';
+import api from "../../services/api";
+import { UserContext } from "../../Providers/User";
 import { Link } from "react-router-dom";
 import { PlusButton } from "../../components/PlusButton";
 import { ContainerGroup } from "./style";
@@ -14,19 +13,19 @@ const Groups = () => {
   const { groups, setGroups } = useContext(GroupsContext);
   const { token } = useContext(UserContext);
 
- 
   useEffect(() => {
     api
-      .get('/groups/', {
+      .get("/groups/", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
       .then((res) => {
-        setGroups(res.data.results)
-        console.log(res)})
+        setGroups(res.data.results);
+        console.log(res);
+      })
       .catch((err) => console.log(err));
-  }, [])
+  }, []);
 
   return (
     <section>
@@ -37,12 +36,22 @@ const Groups = () => {
           <button>meta</button>
         </div>
         <div>
-          {groups.map((item, index) => <Link to={location => ({ ...location, pathname: `/groups/group/${item.id}`})} key={index}><Card type={false} title={item.name} category={item.category} /></Link>)}
+          {groups.map((item, index) => (
+            <Link
+              to={(location) => ({
+                ...location,
+                pathname: `/groups/group/${item.id}`,
+              })}
+              key={index}
+            >
+              <Card type={false} title={item.name} category={item.category} />
+            </Link>
+          ))}
         </div>
         <PlusButton greenSchema={true} />
       </ContainerGroup>
     </section>
-    );
+  );
 };
 
 export default Groups;
