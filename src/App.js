1841a47e1fbdf85b3f39b635326b7f2d6d7afdd9
patrickgsda <patrickarruda30@gmail.com>
2021-10-20 16:59:18 -0300

    feat: test feature card ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import GlobalStyled from "./styles/global";
 import Routes from "./routes";
-import Card from './components/Card';
 
 function App() {
   return (
     <div>
       <GlobalStyled />
       <Routes />
-      <Card type={false} />
     </div>
   );
 }
--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -1,34 +1,34 @@
-import styled from 'styled-components';
-import GroupIcon from '../../assets/icons/groupIcon.svg';
-import HabitIcon from '../../assets/icons/habitIcon.svg';
+import styled from "styled-components";
+import GroupIcon from "../../assets/icons/groupIcon.svg";
+import HabitIcon from "../../assets/icons/habitIcon.svg";
 
 export const Container = styled.div`
-    display: flex;
-    width: 330px;
-    height: 80px;
-    padding: 15px 10px;
-    background-color: var(--white);
+  display: flex;
+  width: 330px;
+  height: 80px;
+  padding: 15px 10px;
+  background-color: var(--white);
 
-    h4 {
-        font-size: 12px;
-        font-weight: 600;
-        line-height: 15px;
-        letter-spacing: -0.017em;
-        color: var(--color-title);
-    }
+  h4 {
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 15px;
+    letter-spacing: -0.017em;
+    color: var(--color-title);
+  }
 
-    span {
-        font-weight: normal;
-        font-size: 10px;
-        line-height: 12px;
-        letter-spacing: -0.017em;
-        color: var(--color-text);
-    }
+  span {
+    font-weight: normal;
+    font-size: 10px;
+    line-height: 12px;
+    letter-spacing: -0.017em;
+    color: var(--color-text);
+  }
 
-    .imgBox {
-        background: ${(props) => props.type ? `url(${HabitIcon})` : `url(${GroupIcon})`};
-        width: 50px;
-        margin-right: 10px;
-    }
-
+  .imgBox {
+    background: ${(props) =>
+      props.type ? `url(${HabitIcon})` : `url(${GroupIcon})`};
+    width: 50px;
+    margin-right: 10px;
+  }
 `;
