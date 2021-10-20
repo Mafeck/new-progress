3b3a437ea3488e03a0bc009de57e984aa5a7840b
marcusvl <marcusvlcastro@gmail.com>
2021-10-20 17:22:22 -0300

    teste

--- src/Providers/Groups/index.js
+++ src/Providers/Groups/index.js
@@ -1,13 +1,9 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useState } from "react";
 
 export const GroupsContext = createContext();
 
 export const GroupsProvider = ({ children }) => {
- 
   const [groups, setGroups] = useState([]);
-
-  
 
   return (
     <GroupsContext.Provider
